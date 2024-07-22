fdca4ef074b368c5b39b75b7ab01364e0041074e
yeraymorenogomez <yeraymorenogomez@gmail.com>
2024-07-22 07:10:07 +0000

    header and footer positioning

--- src/app/Services/api/api-service.service.ts
+++ src/app/Services/api/api-service.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ApiServiceService {
-
+  itemApi:string = "https://api.warframestat.us/items/";
   constructor(private http: HttpClient) {
     // This service can now make HTTP requests via `this.http`.
   }
