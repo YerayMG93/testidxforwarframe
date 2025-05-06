e25c1b01922b8314406691fd6b119da4cf4922b6
yeraymorenogomez <yeraymorenogomez@gmail.com>
2025-05-06 19:08:42 +0000

    Code cleanup and comments

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -20,6 +20,7 @@ export class HomeComponent implements OnInit{
     this.router = router;
   }
 
+  //Checks the resources needed at the start of the app.
   ngOnInit() {
     let items = this.ls.getArrayData('list');
     for (let index = 0; index < items.length; index++) {
@@ -35,18 +36,17 @@ export class HomeComponent implements OnInit{
   //Reads all items from local storage and sums the ressources needed to craft them.
   sumRessources(itemSearch:string){
     let tempItem = this.ls.getObjectData(itemSearch);
-    let tempRes:any;
     if(!tempItem.isCompleted){
       console.log(tempItem);
       let ressources = tempItem.components;
       for (let item of ressources){
-        if(item.name == "Blueprint" && !tempItem.isPrime) item.name = tempItem.name + " " + item.name;
-        else if(tempItem.isPrime == true && !item.productCategory && item.drops != 0) {
-          item.name = item.drops[item.drops.length - 1].location;
-        }else if(tempItem.category == "Warframes" && !tempItem.isPrime && item.name != "Orokin Cell"){
-          console.log(item);
+        //Filter the resource name to show on resource map.
+        if (tempItem.isPrime == true && !item.productCategory && item.drops != 0)item.name = item.drops[item.drops.length - 1].location;
+        else if (item.name == "Blueprint")item.name = tempItem.name + " " + item.name;
+        else if(tempItem.category == "Warframes" && item.name != "Orokin Cell"){
           item.name = tempItem.name + " " + item.name;
         }
+        //Adds to the resource map
         if (this.resourcesNeeded.has(item.name)){
           this.resourcesNeeded.set(item.name, this.resourcesNeeded.get(item.name) + item.itemCount);
         } else {
