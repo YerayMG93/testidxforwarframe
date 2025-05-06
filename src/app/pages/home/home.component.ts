61a3c22647d99b894c269af8310b46a0509c3372
yeraymorenogomez <yeraymorenogomez@gmail.com>
2025-05-06 16:54:29 +0000

    home empty list button functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalService } from '../../Services/local/local.service';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -13,14 +14,19 @@ export class HomeComponent implements OnInit{
   title = 'test';
   resourcesNeeded:Map<string, number> = new Map();
   ls: LocalService;
-  constructor(localStorage: LocalService) {
+  router: Router;
+  constructor(localStorage: LocalService, router: Router) {
     this.ls = localStorage;
+    this.router = router;
   }
   ngOnInit() {
     let items = this.ls.getArrayData('list');
     for (let index = 0; index < items.length; index++) {
       this.sumRessources(items[index]);
     }
+  }
+  goToList(){
+    this.router.navigate(['/list']);
   }
     sumRessources(itemSearch:string){
       let tempItem = this.ls.getObjectData(itemSearch);
